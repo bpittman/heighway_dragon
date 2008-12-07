0a965aafe6c95caad0ea320bdc67f22a7edc34e4
Brian Pittman <shweazel@gmail.com>
2008-12-07 11:25:09 -0600

    Add a naive generate_coords()
    generate_coords() parses the heighway dragon string and returns
    lists containing the x & y coordinates.

--- heighway_dragon.py
+++ heighway_dragon.py
@@ -14,7 +14,42 @@ def generate_string(iterations):
             D = D[:j] + b + D[j+1:]
    return D
 
+def generate_coords(string):
+   facing = 90
+   xcoords = [0]
+   ycoords = [0]
+   for c in string:
+      if c == "a" or c == "b": continue
+      elif c == "L":
+         facing += 90
+         facing = normalize(facing)
+      elif c == "R":
+         facing -= 90
+         facing = normalize(facing)
+      elif c == "F":
+         if facing > 270: print facing
+         if facing < 0: print facing
+         if facing == 0:
+            xcoords.append(xcoords[-1]+1)
+            ycoords.append(ycoords[-1])
+         elif facing == 90:
+            xcoords.append(xcoords[-1])
+            ycoords.append(ycoords[-1]+1)
+         elif facing == 180:
+            xcoords.append(xcoords[-1]-1)
+            ycoords.append(ycoords[-1])
+         elif facing == 270:
+            xcoords.append(xcoords[-1])
+            ycoords.append(ycoords[-1]-1)
+   return xcoords, ycoords
+
+def normalize(x):
+   while x >= 360: x -= 360
+   while x < 0: x += 360
+   return x
+
 if __name__ == "__main__":
    print "D(0) =", generate_string(0)
    print "D(1) =", generate_string(1)
    print "D(2) =", generate_string(2)
+   print generate_coords(generate_string(2))
